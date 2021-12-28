886f32e836cce04580751cd6a26e2c27016e1517
Minji <321963741@naver.com>
2021-12-28 11:07:09 +0900

    onsnapshot까지

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import {async} from '@firebase/util';
 import {initializeApp} from 'firebase/app';
-import {getFirestore, collection, getDocs} from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  deleteDoc,
+  onSnapshot,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD8WuI2siL9wROzTR5_c9ls5qpFgmw0W8Y',
@@ -20,15 +27,46 @@ const db = getFirestore();
 // collection ref
 const collectionRef = collection(db, 'books');
 
-// get collection data
+// get collection data ( 리얼타임 안한걸로 collection data 받아오기)
 const getCollection = async () => {
-  const doc = await getDocs(collectionRef);
-  console.log(doc, 'doc');
+  try {
+    const doc = await getDocs(collectionRef);
+    let books = [];
+    doc.docs.map((item) => {
+      books.push({...item.data(), id: item.id});
+    });
+    console.log(books, 'books');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// realtime으로 바로바로 업뎃시키기
+onSnapshot(collectionRef, (doc) => {
   let books = [];
   doc.docs.map((item) => {
     books.push({...item.data(), id: item.id});
   });
   console.log(books, 'books');
-};
+});
 
-getCollection();
+//adding documents
+const addBookForm = document.querySelector('.add');
+addBookForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const value = await addDoc(collectionRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  });
+  addBookForm.reset();
+  console.log(value, 'value');
+});
+
+//deleting decuments
+const deleteBookForm = document.querySelector('.delete');
+deleteBookForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const deletedRef = doc(db, 'books', deleteBookForm.id.value);
+  await deleteDoc(deletedRef);
+  deleteBookForm.reset();
+});
